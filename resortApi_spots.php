788c5a46d49a0aed6b677c61bbc01efe530f530e
yycgrace <yycgrace84@gmail.com>
2020-09-25 17:05:55 +0800

    wearher travel-info done

---
+++ resortApi_spots.php
@@ -0,0 +1,20 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36");
+
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelStay.aspx
+curl_setopt($curl, CURLOPT_URL, "https://gis.taiwan.net.tw/XMLReleaseALL_public/scenic_spot_C_f.json");
+
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+
+// echo $result;
+
+?>
